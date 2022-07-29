25624e25a9ac10444d8e750760d307b3c015dd72
Prem Patel <80719066+sazncode@users.noreply.github.com>
2022-07-28 22:20:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import math
 import apprise
 from random_words import RandomWords
 os.system("pip install RandomWords")
+
+
 apprise_alerts = os.environ.get("APPRISE_ALERTS", "").split(",")
-
-# Functions
-
+points = 0
 
 def apprise_init():
     alerts = apprise.Apprise()
@@ -36,17 +36,12 @@ def login(EMAIL, PASSWORD, driver):
     driver.find_element(By.XPATH, value='//*[@id="idSIButton9"]').click()
 
 
-def mainLoop():
+def main():
     import time
     rw = "Random"
     # Loop through all accounts doing edge and mobile searches
 
-    def getPoints(EMAIL, PASSWORD):
-        chrome_options = Options()
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-
-        driver = webdriver.Chrome(options=chrome_options)
+    def getPoints(EMAIL, PASSWORD, driver):
         driver.implicitly_wait(3)
         driver.get('https://rewards.microsoft.com/')
         driver.maximize_window()
@@ -62,7 +57,6 @@ def mainLoop():
             print(points)
         except Exception as e:
             print(e)
-        driver.quit()
         return points
     rw = RandomWords()
     EMAIL = os.environ['EMAIL']
@@ -74,15 +68,7 @@ def mainLoop():
     # Loop through the array of accounts, splitting each string into an username and a password, then doing edge and mobile searches
     for x in accounts:
         import time
-        points = getPoints(EMAIL, PASSWORD)
-        alerts.notify(title=f'Bing Rewards:',body=f'Bing Automation Booting...\nPoints: {points}')
-        # Grab username
-        colonIndex = x.index(":")+1
-        user = x[0:colonIndex-1]
-        # Grab password
-        lastIndex = len(x)
-        pw = x[colonIndex:lastIndex]
-        # Edge Searches(34 searches total)
+        
 
         chrome_options = Options()
         chrome_options.add_argument('--no-sandbox')
@@ -90,52 +76,48 @@ def mainLoop():
 
         driver = webdriver.Chrome(options=chrome_options)
         driver.implicitly_wait(3)
+
+        points = getPoints(EMAIL, PASSWORD, driver)
+        alerts.notify(title=f'Bing Rewards:',body=f'Bing Automation Booting...\nPoints: {points}')
+        # Grab username
+        colonIndex = x.index(":")+1
+        user = x[0:colonIndex-1]
+        # Grab password
+        lastIndex = len(x)
+        pw = x[colonIndex:lastIndex]
+
+        # Edge Searches(34 searches total)
         driver.get('https://rewards.microsoft.com/')
         try:
             driver.find_element(By.XPATH, '//*[@id="raf-signin-link-id"]').click()
             login(EMAIL, PASSWORD, driver)
-        except Exception:
-            pass
+        except Exception as e:
+            print(e)
         try:
             time.sleep(5)
-            points = driver.find_element(By.XPATH, '//*[@id="rewardsBanner"]/div/div/div[3]/div[1]/mee-rewards-user-status-item/mee-rewards-user-status-balance/div/div/div/div/div/p[1]/mee-rewards-counter-animation/span').text
-            print(points)
             driver.find_element(By.XPATH, value='//*[@id="rx-user-status-action"]').click()
+            time.sleep(7)
             PC = driver.find_element(By.XPATH, value='//*[@id="userPointsBreakdown"]/div/div[2]/div/div[1]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]').text.replace(" ", "").split("/")
-            
-            if (int(PC[0]) < int(PC[1])):
-                print(PC[0])
-                print(PC[1])
+            if(int(PC[0]) < int(PC[1])):
                 print('PC Needs more searching')
-                Number_PC_Search = (int(PC[1]) - int(PC[0])) / 5
+                Number_PC_Search = int((int(PC[1]) - int(PC[0])) / 5)
                 print(f'{Number_PC_Search} more searches needed.')
             else:
-                Number_PC_Search = 0
-                print(Number_PC_Search[0] + '/' + Number_PC_Search[1] + ': All PC searches complete.')
-
+                print(PC[0] + '/' + PC[1] + ': All PC searches complete.')
+                
             MOBILE = driver.find_element(By.XPATH, value='//*[@id="userPointsBreakdown"]/div/div[2]/div/div[2]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]').text.replace(" ", "").split("/")
             if (int(MOBILE[0]) < int(MOBILE[1])):
-                print('Needs more searching')
-                Number_Mobile_Search = (int(MOBILE[1]) - int(MOBILE[0])) / 5
+                print('Mobile Needs more searching')
+                Number_Mobile_Search = int((int(MOBILE[1]) - int(MOBILE[0])) / 5)
                 print(f'{Number_Mobile_Search} more searches needed.')
             else:
-                Number_Mobile_Search = 0
-                print(MOBILE[0] + '/' + MOBILE[1] +
-                      ': All mobile searches complete.')
+                print(MOBILE[0] + '/' + MOBILE[1] + ': All mobile searches complete.')
         except Exception as e:
             print(e)
+            
+            pass
 
-        try:
-            url = os.environ['URL']
-            driver.get(url)
-            driver.maximize_window()
-            login(EMAIL, PASSWORD, driver)
-        except Exception as e:
-            print(e)
-            print(
-                "Error: Could not log into account. Cross your pinky you're weren't disrespectfully BANNED.")
-            driver.get('https://bing.com')
-
+        driver.get('https://www.bing.com/')
         # First test search
         time.sleep(3)
         first = driver.find_element(By.ID, value = "sb_form_q")
@@ -175,87 +157,87 @@ def mainLoop():
                 print("This is ", end="")
                 print(percentDone, end="")
                 print("% done.")
-            print("Account [" + user + "] has completed PC searches. Please close the window or complete the daily taskes!")
             if (Number_PC_Search > 0):
-                print(f"{Number_PC_Search} left. Starting PC search..")
+                print(f"{Number_PC_Search} PC left. Starting PC search..")
                 search()
+                print("Account [" + user + "] has completed PC searches. Please close the window or complete the daily taskes!")
             else:
-                print(f"{Number_PC_Search} left. Bing Account has no PC searches for the day.")
+                print(f"{Number_PC_Search} PC left. Bing Account has no PC searches for the day.")
+            driver.quit()
 
-            # Opens Mobile Driver
-            mobile_emulation = {"deviceName": "Nexus 5"}
-            chrome_options = webdriver.ChromeOptions()
+            if (Number_Mobile_Search > 0):
+                # Opens Mobile Driver
+                mobile_emulation = {"deviceName": "Nexus 5"}
+                chrome_options = webdriver.ChromeOptions()
 
-            chrome_options = Options()
-            chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--disable-dev-shm-usage')
-            chrome_options.add_experimental_option(
-                "mobileEmulation", mobile_emulation)
+                chrome_options = Options()
+                chrome_options.add_argument('--no-sandbox')
+                chrome_options.add_argument('--disable-dev-shm-usage')
+                chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
 
-            driver = webdriver.Chrome(chrome_options=chrome_options)
-            driver.implicitly_wait(3)
-            driver.get("https://login.live.com/")
-            driver.maximize_window()
+                driver = webdriver.Chrome(chrome_options=chrome_options)
+                driver.implicitly_wait(3)
+                driver.get("https://login.live.com/")
+                driver.maximize_window()
 
-            login(EMAIL, PASSWORD, driver)
-            print("Account [" + user + "] logged in successfully! Auto search initiated.")
-            driver.get('https://www.bing.com/')
+                login(EMAIL, PASSWORD, driver)
+                print("Account [" + user + "] logged in successfully! Auto search initiated.")
+                driver.get('https://www.bing.com/')
+                def mobile():
+                    # I N P U T = = = = = N E E D E D = = = = = = =Input number of searches wanted in 'numSearch' or uncomment #numSearch to input while the code is running
+                    # numSearch = int(input("Please enter number of searches: "))
 
-        def mobile():
-            # I N P U T = = = = = N E E D E D = = = = = = =Input number of searches wanted in 'numSearch' or uncomment #numSearch to input while the code is running
-            # numSearch = int(input("Please enter number of searches: "))
+                    # Main search loop
+                    for x in range(1, Number_Mobile_Search + 1):
+                        value = 'define ' + rw.random_word()
 
-            # Main search loop
-            for x in range(1, Number_Mobile_Search + 1):
-                value = 'define ' + rw.random_word()
+                        # Clear search bar
+                        ping = driver.find_element(By.ID, value = "sb_form_q")
+                        ping.clear()
 
-                # Clear search bar
-                ping = driver.find_element(By.ID, value = "sb_form_q")
-                ping.clear()
+                        # Send random keyword
+                        ping.send_keys(value)
 
-                # Send random keyword
-                ping.send_keys(value)
+                        # add delay to prevent ban
+                        time.sleep(1)
+                        go = driver.find_element(By.ID, value = "sb_form_go")
+                        go.click()
 
-                # add delay to prevent ban
-                time.sleep(1)
-                go = driver.find_element(By.ID, value = "sb_form_go")
-                go.click()
+                        # add delay to prevent ban
+                        time.sleep(delay)
 
-                # add delay to prevent ban
-                time.sleep(delay)
+                        # Print progress after each search
+                        print("Doing ", end="")
+                        print(x, end="")
+                        print(" search out of ", end="")
+                        print(Number_Mobile_Search)
+                        percentDone = x/Number_Mobile_Search*100
+                        print("This is ", end="")
+                        print(percentDone, end="")
+                        print("% done.")
+                    print("Account [" + user + "] has completed mobile searches]")
+                    driver.quit()
+                mobile()
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
 
-                # Print progress after each search
-                print("Doing ", end="")
-                print(x, end="")
-                print(" search out of ", end="")
-                print(Number_Mobile_Search)
-                percentDone = x/Number_Mobile_Search*100
-                print("This is ", end="")
-                print(percentDone, end="")
-                print("% done.")
-            print("Account [" + user + "] has completed mobile searches]")
-        if (Number_Mobile_Search > 0):
-            mobile()
-        else:
-            print(f"{Number_Mobile_Search} mobile searches left. Bing Account has no mobile searches for the day.")
-        alerts.notify(title=f'Bing Rewards Successful', body=f'Points:{getPoints(EMAIL, PASSWORD)}')
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.implicitly_wait(3)
+        points = getPoints(EMAIL, PASSWORD, driver)
+        print(f"{Number_Mobile_Search} mobile searches left. Bing Account has no mobile searches for the day.")
+        
+        alerts.notify(title=f'Bing Rewards Successful', body=f'Points:{points}')
 
 
 while True:
     try:
-        COMP, CELL = 34, 21
-        mainLoop()
-        time.sleep(86400)
+        main()
+        time.sleep(43200)
     except Exception as e:
         print(e)
-        COMP, CELL = 34, 21
         print("Error. Restarting...")
-        alerts.notify(title=f'Bing Rewards',
-                      body='Bing Automation Failed! Restarting!')
+        alerts.notify(title=f'Bing Rewards',body=f'Bing Automation Failed!\n{e}\nRestarting!')
         time.sleep(500)
         continue
 
-
-# Activate main loop
-mainLoop()
-print("BingAutoSearch V 3.0.0 has successfully boosted points on all accounts!")
