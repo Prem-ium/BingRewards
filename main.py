d06a57ea530e74db012ed31a4e0a30ef4b4971db
Prem Patel <80719066+sazncode@users.noreply.github.com>
2022-08-06 10:20:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # LOGIN EXAMPLE:
 # "EMAIL:PASSWORD,EMAIL:PASSWORD"
