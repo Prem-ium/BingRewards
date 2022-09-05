37cf1a11af62dc7d28f83758773fc64cb5678938
Nelson Dane <danenb@clarkson.edu>
2022-09-05 10:14:05 -0400

    Add user agent print

--- main.py
+++ main.py
@@ -507,6 +507,8 @@ def getDriver(isMobile = False):
         # Set edge user agent if not mobile
         user_agent = "mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/64.0.3282.140 safari/537.36 edge/18.17763"
         chrome_options.add_argument(f'user-agent={user_agent}')
+        # For testing
+        print(f"Set Chrome user agent to {user_agent}")
 
     if not HANDLE_DRIVER:
         driver = webdriver.Chrome(options=chrome_options)
