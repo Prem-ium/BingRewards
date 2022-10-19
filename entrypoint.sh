2574f05d7d74560d2ff7b5b555268d328ec56d43
Nelson Dane <danenb@clarkson.edu>
2022-10-19 15:41:38 -0400

    Print browser and driver info at bot start

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@ rm /tmp/.X99-lock
 echo "Starting X virtual framebuffer (Xvfb) in background..."
 Xvfb -ac :99 -screen 0 1280x1024x16 &
 export DISPLAY=:99
+echo "BROWSER: $BROWSER"
+echo "HANDLE_DRIVER: $HANDLE_DRIVER"
 echo "Starting Bing Rewards Bot..."
 python3 main.py
