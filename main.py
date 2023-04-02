bcd2bf7aeef5bbbdcf9b7ca7c02c0955aeca47c2
Rohan Udupa <86579136+lab596@users.noreply.github.com>
2023-04-02 10:10:31 -0700

    Update main.py
    Fixes the warpspeed quiz hang. Was initially using undefined variable called "button"

--- main.py
+++ main.py
@@ -543,7 +543,7 @@ def do_quiz(driver):
                             # wait 5 seconds
                             sleep(5)
                             # get a random answer option
-                            option = driver.find_element(By.XPATH, value=f'//*[@id="rqAnswerOption{random.randint(0, buttons - 1)}"]')
+                            option = driver.find_element(By.XPATH, value=f'//*[@id="rqAnswerOption{random.randint(0, choices - 1)}"]')
                             # click the option
                             option.click()
                             # wait 10 seconds
@@ -551,7 +551,7 @@ def do_quiz(driver):
                             try:
                                 # if the answer was incorrect, choose another option
                                 while driver.find_element(By.XPATH, value='//*[@id="rqAnsStatus"]').text.lower() == 'oops, try again!':
-                                    option = driver.find_element(By.XPATH, value=f'//*[@id="rqAnswerOption{random.randint(0, buttons - 1)}"]')
+                                    option = driver.find_element(By.XPATH, value=f'//*[@id="rqAnswerOption{random.randint(0, choices - 1)}"]')
                                     option.click()
                                     sleep(5)
                             except:
